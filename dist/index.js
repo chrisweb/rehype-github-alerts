bcef2aeb163a9b75341b3dd4e6362f1928c3e898
chrisweb <chris963@gmail.com>
2025-01-08 17:38:57 +0100

    new build

--- dist/index.js
+++ dist/index.js
@@ -9,31 +9,26 @@ const rehypeGithubAlerts = (options) => {
     alerts: [
       {
         keyword: "NOTE",
-        // bootstrap icon: info-circle
         icon: octicons.info.toSVG(),
         title: "Note"
       },
       {
         keyword: "IMPORTANT",
-        // bootstrap icon: exclamation-square
         icon: octicons.report.toSVG(),
         title: "Important"
       },
       {
         keyword: "WARNING",
-        // bootstrap icon: exclamation-triangle
         icon: octicons.alert.toSVG(),
         title: "Warning"
       },
       {
         keyword: "TIP",
-        // bootstrap icon: lightbulb
         icon: octicons["light-bulb"].toSVG(),
         title: "Tip"
       },
       {
         keyword: "CAUTION",
-        // bootstrap icon: exclamation-octagon
         icon: octicons.stop.toSVG(),
         title: "Caution"
       }
